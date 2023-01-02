bd47fbd7d252df7ef4997b73e15e7a7731b9ecc2
isaacthajunior <isaacjunez669@gmail.com>
2023-01-02 11:55:11 +0100

    omoh

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
- 
+
 void main() {
   runApp(const MyApp());
 }
@@ -74,8 +74,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
